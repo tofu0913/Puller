7066cf509a643a44f900ede2124ae8811cdb0fe1
Cliff Chen <cfchen@futuredial.com>
2026-03-26 12:10:03 +0800

    New version of puller

--- Puller.lua
+++ Puller.lua
@@ -10,16 +10,8 @@ require('mylibs/aggro')
 
 
 local enabled = false
-local STATES = {
-	INIT = 0,
-	PULLING = 1,
-	PULLING_CURE = 1.5,
-	BACK = 2,
-	BACK_CURE = 2.5,
-	WAITKILL = 3,
-}
-local state = STATES.INIT
 local timer = os.clock()
+local targets = {}
 
 -- Settings
 config = require('config')
@@ -30,10 +22,11 @@ default = {
 			y = 344
 		}
 	},
-	cure_threshold = 60,
+	-- cure_threshold = 60,
 	monitor_range = 15,
-	pull_max = 5,
-	pull_min = 0,
+	pull_back = true,
+	-- pull_max = 5,
+	-- pull_min = 0,
 }
 settings = config.load(default)
 
@@ -53,79 +46,98 @@ end
 widget = texts.new("${msg}", settings.text_setting, default.text_setting)
 setup_text(widget)
 
-function update_widget(aggr, near)
-	str = 'Agg: '..aggr
-	if near > 0 then
-		str = str .. ' Near'..near
-	end
-	widget.msg = '('..state..')'..str
+function update_widget(aggr)
+	str = 'Ta: '..#targets
+	str = str .. ',Ag: '..aggr
+	widget.msg = str
 end
 
-function scan_nearby_mobs()
-	local count = 0
-	for key,mob in pairs(windower.ffxi.get_mob_array()) do
-		if mob["valid_target"] and mob['spawn_type']==16 and math.sqrt(mob.distance) < settings.monitor_range and mob.hpp > 0 then
-			count = count + 1
-		end
-	end
-	return count
+function tsp_nearest_neighbor(points)
+    local n = #points
+    if n == 0 then return {}, 0 end
+    if n == 1 then return {1}, 0 end
+
+    local visited = {}
+    local path = {}          -- 存放點的索引
+    local total_dist = 0
+
+    local current = 1
+    table.insert(path, current)
+    visited[current] = true
+
+    for _ = 1, n-1 do
+        local min_d = math.huge
+        local next_point = nil
+
+        for i = 1, n do
+            if not visited[i] then
+                local d = xy_get_distance(points[current], points[i])
+                if d < min_d then
+                    min_d = d
+                    next_point = i
+                end
+            end
+        end
+
+        table.insert(path, next_point)
+        total_dist = total_dist + min_d
+        visited[next_point] = true
+        current = next_point
+    end
+
+    return path, total_dist
 end
 
 windower.register_event('prerender', function(...)
 	if os.clock() - timer >= 0.5 then
-		if windower.ffxi.get_player() and windower.ffxi.get_player().vitals.hpp <= settings.cure_threshold then
-			if state == STATES.PULLING or state == STATES.BACK then
-				state = state + 0.5
-				windower.send_command('fsd stop')
-			end
-			windower.send_command(windower.to_shift_jis('input /ma ケアルIV <me>'))
-			timer = os.clock() + 2
-			return
-		else
-			if state == STATES.PULLING_CURE or state == STATES.BACK_CURE then
-				state = state - 0.5
-				windower.send_command('fsd continue')
-			elseif state == STATES.WAITKILL and not hasBuff('プロテス') then
-				windower.send_command(windower.to_shift_jis('input /ma プロテスV <me>'))
-			end
-		end
-
+		
 		local aggr = aggroCount()
-		if state == STATES.PULLING and aggr >= settings.pull_max then
-			state = STATES.BACK
-			log('Pull done, go back')
-			windower.send_command('fsd stop')
-			coroutine.sleep(0.5)
-			main()
-		end
-		local near = scan_nearby_mobs()
-		if state == STATES.WAITKILL and scan_nearby_mobs() <= settings.pull_min then
-			state = STATES.INIT
-			log('No mob, go pull')
-			main()
-		end
-		update_widget(aggr, near)
+		update_widget(aggr)
 		timer = os.clock()
 	end
 end)
 
-function main()
-	if state == STATES.INIT then
-		state = STATES.PULLING
-		waitfor(function()
-			windower.ffxi.cancel_buff(116)
-			coroutine.sleep(2)
-			windower.send_command(windower.to_shift_jis('input /ma ファランクス <me>'))
-			return hasBuff('ファランクス')
-		end, function()
-			fsd_go_loop('puller', 'puller')
-		end)
-	elseif state == STATES.BACK then
-		fsd_go_back('puller', function()
-			log('wait for mobs killed')
-			state = STATES.WAITKILL
-		end)
+function go_path(path, curr)
+	if not enabled then return end
+
+	log(curr..'/'..#path)
+	local node = targets[path[curr]]
+	-- log(dump(node))
+	fsd_to('pu', node.x, node.y, function()
+		if curr+1 <= #path then
+			go_path(path, curr+1)
+		else
+			log('Finished!!!')
+		end
+	end)
+end
+
+function main(middle)
+	targets = {}
+	for key,mob in pairs(windower.ffxi.get_mob_array()) do
+		if mob["valid_target"] and mob['spawn_type']==16 and xy_get_distance(mob, middle) < settings.monitor_range and mob.hpp > 0 then
+			table.insert(targets, {
+				['index'] = mob.index,
+				['id'] = mob.id,
+				['name'] = mob.name,
+				['x'] = mob.x,
+				['y'] = mob.y,
+				['z'] = mob.z,
+			})
+		end
 	end
+	path, d = tsp_nearest_neighbor(targets)
+	if settings.pull_back then
+		local self = windower.ffxi.get_mob_by_index(windower.ffxi.get_player().index)
+		table.insert(targets, {
+			['name'] = 'goal',
+			['x'] = self.x,
+			['y'] = self.y,
+			['z'] = self.z,
+		})
+		table.insert(path, #targets)
+	end
+	go_path(path, 1)
 end
 
 windower.register_event('addon command', function (command, ...)
@@ -133,26 +145,50 @@ windower.register_event('addon command', function (command, ...)
 	local args = T{...}
 
 	if T{"go","g"}:contains(command) then
-		enabled = true
-		state = STATES.INIT
-		log('Let\'s Pull!!!!')
-		main()
+		local target = windower.ffxi.get_mob_by_target('t')
+		if target then
+			enabled = true
+			log('Let\'s Pull!!!!')
+			main(target)
+		else
+			windower.add_to_chat(2, 'Not target...')
+		end
 	elseif T{"stop","s"}:contains(command) then
 		enabled = false
+		windower.send_command('fsd stop')
 		log('Stopped')
+	elseif T{"trigger"}:contains(command) then
+		if enabled then
+			windower.send_command('pu stop')
+		else
+			windower.send_command('pu go')
+		end
+
+	elseif T{"dist"}:contains(command) then
+		if args[1] then
+			settings.monitor_range = tonumber(args[1])
+		end
+        windower.add_to_chat(11,"Dist: "..settings.monitor_range)
+	elseif T{"back"}:contains(command) then
+		if args[1] then
+			settings.pull_back = T{'1','on'}:contains(args[1])
+		else
+			settings.pull_back = not settings.pull_back
+		end
+        windower.add_to_chat(11,"Pull back: "..tostring(settings.pull_back))
+
 	elseif command == 'save' then
 		log('Settings saved')
 		settings:save()
-
-	elseif command == 'debug' then
-		log(state)
 	end
 end)
 
 windower.register_event('load', function()
     log('===========loaded===========')
+    windower.send_command('bind @p input //pu trigger')
 end)
 
 windower.register_event('unload', function()
     windower.send_command('fsd stop')
+    windower.send_command('unbind @p')
 end)
